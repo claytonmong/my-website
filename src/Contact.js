63c92d62669eb6e969ee241d1941d2d4b4337b65
Clayton Mong <mong.36@osu.edu>
2021-03-01 21:39:30 -0500

    Added basic pages

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+            <h1>THIS IS THE CONTACT ME PAGE</h1>
+        </div>
+
+    );
+
+}
+
+export default Contact;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <h1>THIS IS THE HEADER</h1>
+        </div>
+
+    );
+
+}
+
+export default Header;
